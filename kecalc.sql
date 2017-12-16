7fde11853ec49c201df125b76119bd6529034ca1
Christoph Scholz <christoph.scholz@gmail.com>
2017-12-16 09:10:35 +0100

    added autoincrement

--- kecalc.sql
+++ kecalc.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.6
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Erstellungszeit: 15. Dez 2017 um 17:02
--- Server-Version: 10.1.29-MariaDB
--- PHP-Version: 7.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Datenbank: `kecalc`
---
-
--- --------------------------------------------------------
-
 --
 -- Tabellenstruktur für Tabelle `menu`
 --
@@ -39,15 +13,13 @@ CREATE TABLE `menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indizes der exportierten Tabellen
---
-
---
 -- Indizes für die Tabelle `menu`
 --
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`me_id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT für Tabelle `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `me_id` int(11) NOT NULL AUTO_INCREMENT;
