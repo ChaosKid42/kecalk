ca301bbc4923775d688494da22a6a3abbb0d13b0
Christoph Scholz <christoph.scholz@gmail.com>
2017-12-17 10:49:29 +0100

    added apache config

--- kecalc.py
+++ kecalc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, abort, jsonify
+from flask import Flask, jsonify
 import MySQLdb
 
 from dateconverter import DateConverter
@@ -8,18 +8,38 @@ from dateconverter import DateConverter
 app = Flask(__name__)
 app.url_map.converters['date'] = DateConverter
 
-
 # Test: http://localhost:5000/menu/2017-12-11
+# Test: http://localhost:5000/params
+
+DBNAME = 'kecalc'
+DBUSER = 'kecalc'
+DBPASS = 'k8Calc'
 
 @app.route('/menu/<date:date>', methods=['GET'])
 def menu(date):
-  db = MySQLdb.connect(user='kecalc', passwd='k8Calc', db='kecalc')
+  db = MySQLdb.connect(user=DBUSER, passwd=DBPASS, db=DBNAME)
   c = db.cursor(MySQLdb.cursors.DictCursor)
   c.execute('''SELECT `me_food`, `me_measure`,
    `me_amount`, `me_ke` FROM `menu` WHERE me_date=%s
    ORDER BY me_date_id ASC''', (date,))
+  result = c.fetchall()
+  c.close()
+  db.close()
   
-  return jsonify(c.fetchall())
+  return jsonify(result)
+
+@app.route('/params', methods=['GET'])
+def params():
+  db = MySQLdb.connect(user=DBUSER, passwd=DBPASS, db=DBNAME)
+  c = db.cursor(MySQLdb.cursors.DictCursor)
+  c.execute('SELECT `pa_name`, `pa_value` FROM `params`')
+  result = {}
+  for row in c:
+    result[row['pa_name']] = row['pa_value']
+  c.close()
+  db.close()
+
+  return jsonify(result)
 
 # run the app for debug purposes.
 if __name__ == '__main__':
--- kecalc.sql
+++ kecalc.sql
@@ -13,13 +13,36 @@ CREATE TABLE `menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Tabellenstruktur für Tabelle `params`
+--
+
+CREATE TABLE `params` (
+  `pa_id` int(11) NOT NULL,
+  `pa_name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pa_value` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+--
 -- Indizes für die Tabelle `menu`
 --
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`me_id`);
 
 --
+-- Indizes für die Tabelle `params`
+--
+ALTER TABLE `params`
+  ADD PRIMARY KEY (`pa_id`);
+
+--
 -- AUTO_INCREMENT für Tabelle `menu`
 --
 ALTER TABLE `menu`
   MODIFY `me_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `params`
+--
+ALTER TABLE `params`
+  MODIFY `pa_id` int(11) NOT NULL AUTO_INCREMENT;
