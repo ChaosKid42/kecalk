f69be2ba773de32877864d896ed80bf436d3f025
Christoph Scholz <christoph.scholz@gmail.com>
2017-12-17 10:42:46 +0100

    limited length of VARCHAR fields

--- kecalc.sql
+++ kecalc.sql
@@ -6,8 +6,8 @@ CREATE TABLE `menu` (
   `me_id` int(11) NOT NULL,
   `me_date` date NOT NULL,
   `me_date_id` int(11) NOT NULL,
-  `me_food` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `me_measure` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `me_food` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `me_measure` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
   `me_amount` int(11) NOT NULL,
   `me_ke` double(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
